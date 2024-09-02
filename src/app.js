99cbd7e65e265ad305d6fea7517b7c49947325e0
Diego Vázquez <dvazquezlagreca@gmail.com>
2024-09-02 20:53:46 -0300

    Add Cors Lib
    Se instala y usa cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";  // Import the cors package
+
 import errorHandler from "./middlewares/error-handler.mjs";
 import { requestLogger } from "./middlewares/request-logger.mjs";
 import tasksRouter from "./routes/tasks.mjs";
 
 const app = express();
 const port = 3000;
+// Use CORS middleware
+app.use(cors());
 
 app.use(express.json());
 
